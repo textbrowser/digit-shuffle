9f568a36230b3a2def43377d1399cfaaad129e18
textbrowser@gmail.com <textbrowser@gmail.com>
2021-07-06 07:35:19 -0400

    Solve.

--- digit-shuffle.h
+++ digit-shuffle.h
@@ -49,16 +49,19 @@ class digit_shuffle: public QMainWindow
     const int e = solutions.at(4);
     const int f = solutions.at(5);
 
-    for(int x1 = 1; !m_future.isCanceled() && x1 <= 9; x1++)
-      for(int x2 = 1; !m_future.isCanceled() && x2 <= 9; x2++)
-	for(int x3 = 1; !m_future.isCanceled() && x3 <= 9; x3++)
-	  for(int x4 = 1; !m_future.isCanceled() && x4 <= 9; x4++)
-	    for(int x5 = 1; !m_future.isCanceled() && x5 <= 9; x5++)
-	      for(int x6 = 1; !m_future.isCanceled() && x6 <= 9; x6++)
-		for(int x7 = 1; !m_future.isCanceled() && x7 <= 9; x7++)
-		  for(int x8 = 1; !m_future.isCanceled() && x8 <= 9; x8++)
-		    for(int x9 = 1; !m_future.isCanceled() && x9 <= 9; x9++)
+    for(int x1 = 1; x1 <= 9; x1++)
+      for(int x2 = 1; x2 <= 9; x2++)
+	for(int x3 = 1; x3 <= 9; x3++)
+	  for(int x4 = 1; x4 <= 9; x4++)
+	    for(int x5 = 1; x5 <= 9; x5++)
+	      for(int x6 = 1; x6 <= 9; x6++)
+		for(int x7 = 1; x7 <= 9; x7++)
+		  for(int x8 = 1; x8 <= 9; x8++)
+		    for(int x9 = 1; x9 <= 9; x9++)
 		      {
+			if(m_future.isCanceled())
+			  goto done_label;
+
 			// Columns!
 
 			int c1 = x1;
@@ -292,21 +295,23 @@ class digit_shuffle: public QMainWindow
 				<< x8
 				<< x9;
 
-			    if(list.size() != set.size())
-			      continue;
-
-			    results << x1
-				    << x2
-				    << x3
-				    << x4
-				    << x5
-				    << x6
-				    << x7
-				    << x8
-				    << x9;
-			    break;
+			    if(list.size() == set.size())
+			      {
+				results << x1
+					<< x2
+					<< x3
+					<< x4
+					<< x5
+					<< x6
+					<< x7
+					<< x8
+					<< x9;
+				goto done_label;
+			      }
 			  }
 		      }
+
+  done_label:
 
     if(!m_future.isCanceled())
       emit computed(results);
